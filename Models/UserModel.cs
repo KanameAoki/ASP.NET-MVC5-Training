9ad7c4b2ec3f90c04ca7052e493feb5c36cc0950
KanameAoki <ju789olkj@gmail.com>
2023-02-08 13:12:47 +0900

    Add Application1 as a new project

---
+++ Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace WebApplication2.Models
+{
+    public class UserModel
+    {
+        /// DisplayName属性でプロパティの名前が表示されるようになる
+        [DisplayName("ユーザーID")]
+        public string UserID { get; set; }
+
+        [DisplayName("パスワード")]
+        public string Password { get; set;}
+    }
+}
